e45a3f76b88179c41db13281d42e69ec74ce4733
bedieber_cp <SND\bedieber_cp>
2011-02-24 12:44:04 +0000

    Added License headers to source files

--- nEMO/trunk/nEMO/Selection/BasicMultiCriterionSelection.cs
+++ nEMO/trunk/nEMO/Selection/BasicMultiCriterionSelection.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//=============================================================================
+// Project  : nEMO .Net Evolutionary Multiobjective Optimization Framework
+// File    : BasicMultiCriterionSelection.cs
+// Author  : Bernhard Dieber (Bernhard.Dieber@uni-klu.ac.at)
+// Copyright 2011 by Bernhard Dieber
+// This code is published under the Microsoft Public License (Ms-PL).  A copy
+// of the license should be distributed with the code.  It can also be found
+// at the project website: http://nemo.CodePlex.com.   This notice, the
+// author's name, and all copyright notices must remain intact in all
+// applications, documentation, and source files.
+//=============================================================================
+using System;
 using System.Collections.Generic;
 using nEMO.Algorithm;
 
